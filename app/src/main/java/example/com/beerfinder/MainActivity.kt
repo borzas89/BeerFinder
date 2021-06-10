a53b4acc00098cdf3db1fa894cfd9840ba842376
borzas89 <borza.zsolt89@gmail.com>
2021-06-10 16:41:26 +0200

    organizing dependencies, adding Hilt
    organizing dependencies, adding Hilt, adding UI elements

--- app/src/main/java/example/com/beerfinder/MainActivity.kt
+++ app/src/main/java/example/com/beerfinder/MainActivity.kt
@@ -2,10 +2,42 @@ package example.com.beerfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import example.com.beerfinder.databinding.ActivityMainBinding
+import example.com.beerfinder.extension.Navigator
+import example.com.beerfinder.ui.list.MarkerListFragment
 
-class MainActivity : AppCompatActivity() {
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), Navigator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            add(MarkerListFragment())
+        }
+    }
+
+    override fun add(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainContent, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun pop() {
+        supportFragmentManager.popBackStack()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            pop()
+        } else {
+            finish()
+        }
     }
 }
